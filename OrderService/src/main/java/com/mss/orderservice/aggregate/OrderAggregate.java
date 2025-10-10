2ee4425b6a5cb39340befc1c77b985191dba6585
giap.thaidinh <giaptdhe186094@fpt.edu.vn>
2025-10-10 16:09:20 +0700

    Final code

--- OrderService/src/main/java/com/mss/orderservice/aggregate/OrderAggregate.java
+++ OrderService/src/main/java/com/mss/orderservice/aggregate/OrderAggregate.java
@@ -11,8 +11,10 @@ import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
 import org.axonframework.modelling.command.AggregateLifecycle;
+import org.axonframework.spring.stereotype.Aggregate;
 import org.springframework.beans.BeanUtils;
 
+@Aggregate
 public class OrderAggregate {
     @AggregateIdentifier
     private String orderId;
